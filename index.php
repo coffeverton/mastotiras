84e9f104f1770a50aa341a3577f000c30db3d183
Everton Pereira <suporte@agencialink.com>
2016-04-29 08:11:50 -0300

    adicionando captionthis

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $loader = require 'vendor/autoload.php'; 
 
-$calvin = new \Tiras\Calvin();
-$dilbert = new \Tiras\Dilbert();
-$hagar = new \Tiras\Hagar();
+$calvin         = new \Tiras\Calvin();
+$dilbert        = new \Tiras\Dilbert();
+$hagar          = new \Tiras\Hagar();
+$captionthis    = new \Tiras\Captionthis();
---
+++ src/Tiras/Captionthis.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tiras;
+
+/**
+ * Description of Hagar
+ *
+ * @author everton
+ */
+class Captionthis extends Tiras{
+    
+    public function setName(){
+        $this->name = 'captionthis';
+    }
+    
+    public function generateData(){
+        $inicio = new \DateTime('2016-04-29');
+        $hoje = new \DateTime;
+        $diff = $inicio->diff($hoje);
+        $this->data = (12330 - $diff->days);
+    }
+    
+    public function generateUrl(){
+        $this->url = 'http://www.captionthis.org/image.php?pic_id='.$this->data;
+    }
+    
+    public function get(){
+        return $this->url;
+    }
+    
+    public function process($html){
+        return $html;
+    }
+}
